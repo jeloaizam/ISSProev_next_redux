15b0fc0756df412e64df16047198a87067bc893d
codemente <codementehcg@gmail.com>
2023-01-24 00:32:50 -0500

    First commit in Next-tailwind 24 de enero 2023

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [
+        // ...
+        // require('tailwind-scrollbar'),
+      ],
+
+    },
+    autoprefixer: {},
+  },
+}
